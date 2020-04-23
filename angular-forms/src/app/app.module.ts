60368ac532d22969d36526884fd8584983855b9f
ohey <daniel.seunghyun.oh@gmail.com>
2020-04-23 20:57:57 +1000

    Angular Form Basics

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserSettingsFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
